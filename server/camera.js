1df218db490c0572c232785a793cc5d8ca6948d3
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2018-10-30 11:19:36 +0100

    :sparkles: track my face (#4)

--- server/camera.js
+++ server/camera.js
@@ -2,6 +2,14 @@ const cv = require('opencv4nodejs')
 
 const FPS = 30
 let camera
+const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2)
+
+const tracker = new cv.TrackerBoosting()
+
+let faceDetected = false
+
+const getDate = () => Math.round(Date.now() / 1000)
+let date = getDate()
 
 const isOpen = () => {
   return camera && !camera.read().empty
@@ -18,6 +26,30 @@ const open = async () => {
 const decodeFrame = async () => {
   const img = await camera.readAsync()
   if (img.empty) return
+
+  const gray = await img.bgrToGrayAsync()
+
+  const { objects, levelWeights } = await classifier.detectMultiScaleWithRejectLevelsAsync(gray)
+
+  if (date !== getDate()) {
+    date = getDate()
+    const bestDetection = objects
+      .map((o, i) => ({ rect: o, level: levelWeights[i] }))
+      .reduce((acc, curr) => (acc.level > curr.level ? acc : curr), {})
+
+    if (bestDetection.rect) {
+      faceDetected = true
+      tracker.init(img, bestDetection.rect)
+    }
+
+    if (!bestDetection.rect) {
+      faceDetected = false
+      tracker.clear()
+    }
+  }
+
+  if (faceDetected) img.drawRectangle(tracker.update(img))
+
   return cv.imencode('.jpg', img).toString('base64')
 }
 
